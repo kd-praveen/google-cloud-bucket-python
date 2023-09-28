5f8f55e1d04cdb87769a9be5709a490b149f8651
Praveen K D <praveen.kd@neoastra.com>
2023-09-28 17:06:56 +0530

    delete gcp folder implemented

---
+++ gcp_deleteFolder.py
@@ -0,0 +1,20 @@
+from google.cloud import storage
+import os
+
+# set key credentials file path
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/praveenkd/Documents/Docs/Project Documents/Jio-events/project-jiogamesnow-nonprod-394d3b1e638e.json'
+
+def delete_folder(bucket_name, folder_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    
+    # List all objects in the folder
+    blobs = bucket.list_blobs(prefix=folder_name)
+    
+    # Delete each object in the folder
+    for blob in blobs:
+        blob.delete()
+
+    print(f"Folder {folder_name} deleted.")
+    
+delete_folder('jio-events', 'events-1')
