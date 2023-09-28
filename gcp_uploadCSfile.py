bdce2130b84f077d9bef99920956fb132f02732d
Praveen K D <praveen.kd@neoastra.com>
2023-09-28 14:28:18 +0530

    Temporary presigned url implemented

--- gcp_uploadCSfile.py
+++ gcp_uploadCSfile.py
@@ -1,6 +1,6 @@
 # import packages
 from google.cloud import storage
-import os
+import os, datetime
 
 # set key credentials file path
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/praveenkd/Documents/Docs/Project Documents/Jio-events/project-jiogamesnow-nonprod-394d3b1e638e.json'
@@ -14,6 +14,13 @@ def upload_cs_file(bucket_name, source_file_name, destination_file_name):
     blob = bucket.blob(destination_file_name)
     blob.upload_from_filename(source_file_name)
 
-    return True
+    url = blob.generate_signed_url(
+        # This URL will be valid for 1 hour
+        expiration=datetime.timedelta(hours=1),
+        # Allow GET requests using this URL
+        method='GET'
+    )
+
+    return url
 
 print(upload_cs_file('jio-events', '/home/praveenkd/Documents/Docs/Project Documents/Jio-events/pexels-emma-bauso-3585798.jpg', 'jpeg/wedding-image1.jpg'))
